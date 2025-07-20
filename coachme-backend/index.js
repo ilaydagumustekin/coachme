fa133a95daec97e14f6b590f9044b8574829463e
ilaydagumustekin <ilaydagumustekin216@gmail.com>
2025-07-20 21:59:50 +0300

    CORS updated

--- coachme-backend/index.js
+++ coachme-backend/index.js
@@ -8,24 +8,12 @@ const userRoutes = require("./routes/userRoutes.js");
 const programRoutes = require("./routes/programRoutes.js");
 const adminRoutes = require("./routes/adminRoutes.js");
 
-const whitelist = process.env.CORS_WHITELIST_DOMAINS
-  ? process.env.CORS_WHITELIST_DOMAINS.split(',').map(domain => domain.trim())
-  : [];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // İstek gönderen origin whitelist içinde mi kontrol et
-      if (whitelist.indexOf(origin) !== -1 || !origin) {
-        // origin bulunuyorsa veya null (aynı kaynak isteği gibi) ise izin ver
-        callback(null, true);
-      } else {
-        // Aksi takdirde hata döndür
-        callback(new Error('Bu origin CORS tarafından engellendi.'));
-      }
-    },
-    credentials: true, // Eğer cookie/jwt falan kullanıyorsanız bu true olmalı
-    optionsSuccessStatus: 200 // Bazı eski tarayıcılar (IE11, çeşitli SmartTV'ler) 204 yerine 200 bekler
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    optionsSuccessStatus: 204,
   })
 );
 
